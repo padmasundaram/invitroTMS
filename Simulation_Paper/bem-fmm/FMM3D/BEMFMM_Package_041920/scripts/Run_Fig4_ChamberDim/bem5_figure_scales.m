6ea7ed2a4eed51ca83b23df1e400f0834357babc
Padma Sundaram <tottochan@gmail.com>
2024-09-14 17:09:38 -0400

    Add files via upload

---
+++ Simulation_Paper/bem-fmm/FMM3D/BEMFMM_Package_041920/scripts/Run_Fig4_ChamberDim/bem5_figure_scales.m
@@ -0,0 +1,17 @@
+% bem5a_figure_scales
+
+A = 40;
+
+Emax = A;
+Emin = -A;
+
+Eprimax = A;
+Eprimin = -A;
+
+n_ELevels = 40;
+
+%%  Define dimensionless radius of the integration sphere (for precise integration near boundaries)
+R = 2;
+
+%%  Define observation points in the cross-section (MsxMs observation points)
+Ms = 800;
---
+++ Simulation_Paper/bem-fmm/FMM3D/BEMFMM_Package_041920/scripts/Run_Fig4_ChamberDim/main_script.m
@@ -0,0 +1,12 @@
+
+% main_script
+
+chamber_dim = 135; %15; %45; %135; %<---- SELECT CHAMBER DIMENSION HERE
+
+bem0_load_model;
+bem1_setup_coil;
+bem2_charge_engine;
+bem4_define_planes;
+bem5_figure_scales;
+bem5_volume_XY;
+
