810c93a27c2fba8d1ad16413ecbe682dd834fbba
Padma Sundaram <tottochan@gmail.com>
2024-09-14 12:00:28 -0400

    Add files via upload

---
+++ Simulation_Paper/bem-fmm/FMM3D/BEMFMM_Package_041920/scripts/Run_Fig3_Chamber_Immersion/main_script.m
@@ -0,0 +1,14 @@
+
+% main_script
+% Comment: Just run this script and it will do everything
+%
+
+bem0_load_model;
+bem1_setup_coil;
+bem2_charge_engine;
+
+bem4_define_planes;
+bem5_figure_scales;
+
+bem5_volume_XY;
+bem5_volume_YZ;
